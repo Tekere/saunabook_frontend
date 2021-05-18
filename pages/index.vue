bf04f988b26b49bfb56a1fd7901be69c7269175f
SumimotoTakara <96.sumimoto@gmail.com>
2021-05-18 11:11:57 +0900

    ファイル作成等

--- pages/index.vue
+++ pages/index.vue
@@ -4,27 +4,13 @@
       <Logo />
       <h1 class="title">sauna-book_frontend</h1>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <router-link to="saunas">saunas</router-link>
+        <router-link to="users">users</router-link>
+        <router-link to="visit-record">visit-record</router-link>
       </div>
     </div>
   </div>
 </template>
-
 <script>
 export default {}
 </script>
